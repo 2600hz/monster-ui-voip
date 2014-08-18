20527906ed039a0ccac46f36402b1c9042797423
Maxime Roux <max@2600hz.com>
2014-08-18 16:49:36 -0700

    UI-634: removed the start_date from temporal rules so the backend sets it to a date in the past

--- submodules/strategy/strategy.js
+++ submodules/strategy/strategy.js
@@ -1037,7 +1037,6 @@ define(function(require){
 							holidayRule = {
 								cycle: "yearly",
 								interval: 1,
-								start_date: monster.util.dateToGregorian(new Date()),
 								month: parseInt(month),
 								type: "main_holidays"
 							};
@@ -2087,7 +2086,6 @@ define(function(require){
 											interval: 1,
 											name: val,
 											type: "main_weekdays",
-											start_date: monster.util.dateToGregorian(new Date()),
 											time_window_start: "32400", // 9:00AM
 											time_window_stop: "61200",  // 5:00PM
 											wdays: [val.substring(4).toLowerCase()]
@@ -2112,7 +2110,6 @@ define(function(require){
 										interval: 1,
 										name: "MainLunchHours",
 										type: "main_lunchbreak",
-										start_date: monster.util.dateToGregorian(new Date()),
 										time_window_start: "43200",
 										time_window_stop: "46800",
 										wdays: self.weekdays
