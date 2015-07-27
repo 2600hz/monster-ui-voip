a01229151c18dca4a241a746c323dc2d9f32d1ea
Joris Tirado <joris.tirado@gmail.com>
2015-07-27 13:27:11 -0700

    Add Spanish to the list of supported languages for Smart PBX

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ define(function(require){
 		i18n: { 
 			'en-US': { customCss: false },
 			'fr-FR': { customCss: false },
-			'ru-RU': { customCss: false }
+			'ru-RU': { customCss: false },
+			'es-ES': { customCss: false }
 		},
 
 		requests: {},
