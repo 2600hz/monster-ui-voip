872e488bc568c5f7874401c9edb55099628b78ba
Jean-Roch Maitre <jr@2600hz.com>
2015-11-18 11:20:17 -0800

    UI-1922: Fixed typo with call recording featur

--- submodules/users/users.js
+++ submodules/users/users.js
@@ -1262,7 +1262,7 @@ define(function(require){
 							currentUser: currentUser
 						});
 					} else {
-						monster.ui.alert('error', self.i18n.active().users.call_recording.noNumber);
+						monster.ui.alert('error', self.i18n.active().users.callRecording.noNumber);
 					}
 				});
 			});
