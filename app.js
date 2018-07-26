730bbba2edd3b8c929efb3ac8a3aa07d3adf9f9a
Joris Tirado <joris@tirado.com>
2018-07-26 11:06:52 -0700

    UI-3110: Add German translation (#75)
    * Add German translation
    
    * Expose German language as a selectable option

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ define(function(require) {
 		css: [ 'app' ],
 
 		i18n: {
+			'de-DE': { customCss: false },
 			'en-US': { customCss: false },
 			'fr-FR': { customCss: false },
 			'ru-RU': { customCss: false },
