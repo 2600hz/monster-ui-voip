fd47de21e19a54c34a204cd7ea314217d7bc89b8
Joris Tirado <joris@tirado.com>
2019-10-28 11:03:55 -0700

    Use correct module name for feature code module

--- submodules/strategy/strategy.js
+++ submodules/strategy/strategy.js
@@ -46,7 +46,7 @@ define(function(require) {
 				name: 'directed_ext_pickup',
 				number: '87',
 				pattern: '^\\*87([0-9]+)$',
-				moduleName: 'group_pickup_feature',
+				moduleName: 'directed_ext_pickup',
 				extraData: {
 					type: 'extension'
 				}
