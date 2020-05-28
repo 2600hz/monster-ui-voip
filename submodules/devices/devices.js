4cbddbb1c5920afc69926b405fa3bee7564b5054
Joris Tirado <joris@tirado.com>
2020-05-27 19:28:38 -0700

    Refactor key entries structure
    While key entries is provided as an object where the key corresponds to
    the entrie's number, this complexity does not have to make it to the
    template.
    
    We already fill up empty entries by generating an array of key entries
    length, hence no need to switch back to an object structure.

--- submodules/devices/devices.js
+++ submodules/devices/devices.js
@@ -1008,8 +1008,7 @@ define(function(require) {
 									.chain(data.template)
 									.get([type, 'iterate'], 0)
 									.range()
-									.keyBy()
-									.mapValues(function(index) {
+									.map(function(index) {
 										return _.get(data.device, ['provision', type, index], {
 											type: 'none'
 										});
@@ -1088,8 +1087,8 @@ define(function(require) {
 										.value(),
 									data: _
 										.chain(mergedDevice)
-										.get(['provision', type], {})
-										.mapValues(function(metadata) {
+										.get(['provision', type], [])
+										.map(function(metadata) {
 											var value = _.get(metadata, 'value', {});
 
 											return _.merge({}, metadata, _.isPlainObject(value)
