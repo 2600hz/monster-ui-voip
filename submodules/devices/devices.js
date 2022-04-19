267e816c79d282cc841e7ae92a9afe979f988b4d
Mauricio Martinez <mauricio.mart9@gmail.com>
2022-04-19 11:30:50 -0600

    fix payload schema

--- submodules/devices/devices.js
+++ submodules/devices/devices.js
@@ -1237,7 +1237,9 @@ define(function(require) {
 						caller_id_options: {
 							outbound_privacy: "none"
 						},
-						ignore_completed_elsewhere: false,
+						sip: _.merge({
+							ignore_completed_elsewhere: false,
+						}, _.pick(sipSettings.sip, ['password', 'username'])),
 						media: {
 							webrtc: false,
 							encryption: {
