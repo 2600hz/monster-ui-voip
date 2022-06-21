710852608380ae73a706921a52522b39e9afae73
Mauricio Martínez <mauricio.mart9@gmail.com>
2022-06-21 10:54:43 -0600

    MSPB-251: fix parseTime function (#423)
    * MSPB-251: fix parseTime function
    
    * use moment on parseTime function
    
    Co-authored-by: Joris Tirado <joris@tirado.com>

--- submodules/strategyHours/strategyHours.js
+++ submodules/strategyHours/strategyHours.js
@@ -2,6 +2,7 @@ define(function(require) {
 	var monster = require('monster'),
 		_ = require('lodash'),
 		$ = require('jquery'),
+		moment = require('moment'),
 		Papa = require('papaparse'),
 		timezone = require('monster-timezone');
 
@@ -170,10 +171,9 @@ define(function(require) {
 					_.toLower
 				),
 				parseTime = function(time) {
-					return time <= 24 ? time * meta.unit : time;
+					return moment.duration(time).asSeconds();
 				},
 				sanitizeTime = _.flow(
-					_.toNumber,
 					parseTime,
 					_.floor
 				),
