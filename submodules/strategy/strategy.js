5645574271bf6d55ca90c797607d4ad843d5dd39
Guillermo Gutiérrez <guillegr123@gmail.com>
2023-03-01 16:43:05 -0600

    MSPB-324: Allow to list and delete expired holidays (#483)

--- submodules/strategy/strategy.js
+++ submodules/strategy/strategy.js
@@ -4337,9 +4337,9 @@ define(function(require) {
 				event.preventDefault();
 
 				var $this = $(this),
-					className = $this.val(),
+					selectedDateType = $this.val(),
 					isRecurringChecked = $template.find('#recurring').prop('checked'),
-					$singleDateYearElement = $template.find('.optional-year'),
+					$optionalYearElement = $template.find('.optional-year'),
 					$recurringElement = $template.find('#recurring');
 
 				$template
@@ -4347,18 +4347,18 @@ define(function(require) {
 					.removeClass('selected');
 
 				$template
-					.find('.row-fluid.' + className)
+					.find('.row-fluid.' + selectedDateType)
 					.addClass('selected');
 
-				if ((className === 'single' && isRecurringChecked) || className !== 'single') {
-					$singleDateYearElement
+				if (selectedDateType === 'advanced' || isRecurringChecked) {
+					$optionalYearElement
 						.removeClass('show');
-				} else if (className === 'single' && !isRecurringChecked) {
-					$singleDateYearElement
+				} else {
+					$optionalYearElement
 						.addClass('show');
 				}
 
-				if (className === 'advanced') {
+				if (selectedDateType === 'advanced') {
 					$recurringElement
 						.prop('checked', true);
 
@@ -4369,12 +4369,11 @@ define(function(require) {
 						.removeAttr('disabled');
 				}
 
-				if (['advanced', 'range'].includes(className)) {
+				if (['advanced', 'range'].includes(selectedDateType)) {
 					$template
 						.find('.optional-time')
 						.removeClass('show');
 				}
-
 			});
 
 			$template.on('click', '#all_day', function() {
@@ -4471,10 +4470,6 @@ define(function(require) {
 						? value + 1
 						: value;
 				});
-
-				if (!formData.recurring) {
-					holidayData.endYear = args.yearSelected;
-				}
 
 				holidayData.name = formData.name;
 				holidayData.recurring = formData.recurring;
